f79d03cbe23e040a365d1d6e3a13f718e070d5e3
Saaketh Bhojanam <saaki.bhojanam@gmail.com>
2025-10-22 17:42:44 -0500

    Initial Commit

---
+++ Good-Nap/Good_NapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_NapApp.swift
+//  Good-Nap
+//
+//  Created by AI Club on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Good_NapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Good-NapTests/Good_NapTests.swift
@@ -0,0 +1,16 @@
+//
+//  Good_NapTests.swift
+//  Good-NapTests
+//
+//  Created by AI Club on 10/22/25.
+//
+
+import Testing
+
+struct Good_NapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
